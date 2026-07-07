9495043d5217669c76bf1f5ea0be42a1f070c679
Tanutchakorn <tanutchakornkhaisaeng@gmail.com>
2026-07-07 17:44:07 +0700

    Fix rainbow-delimiters config to use new API

--- nvim/.config/nvim/lua/plugins/rainbow-delimiters.lua
+++ nvim/.config/nvim/lua/plugins/rainbow-delimiters.lua
@@ -1,25 +1,23 @@
 return {
   "HiPhish/rainbow-delimiters.nvim",
-  config = function()
-    local rainbow = require("rainbow-delimiters")
-    vim.g.rainbow_delimiters = {
-      strategy = rainbow.strategy.global,
-      query = {
-        javascript = "rainbow-delimiters",
-        typescript = "rainbow-delimiters",
-        lua = "rainbow-delimiters",
-        python = "rainbow-delimiters",
-        html = "rainbow-delimiters",
-      },
-      highlight = {
-        "RainbowDelimiterRed",
-        "RainbowDelimiterYellow",
-        "RainbowDelimiterBlue",
-        "RainbowDelimiterOrange",
-        "RainbowDelimiterGreen",
-        "RainbowDelimiterViolet",
-        "RainbowDelimiterCyan",
-      },
-    }
-  end,
+  main = "rainbow-delimiters.setup",
+  opts = {
+    strategy = { [""] = "rainbow-delimiters.strategy.global" },
+    query = {
+      javascript = "rainbow-delimiters",
+      typescript = "rainbow-delimiters",
+      lua = "rainbow-delimiters",
+      python = "rainbow-delimiters",
+      html = "rainbow-delimiters",
+    },
+    highlight = {
+      "RainbowDelimiterRed",
+      "RainbowDelimiterYellow",
+      "RainbowDelimiterBlue",
+      "RainbowDelimiterOrange",
+      "RainbowDelimiterGreen",
+      "RainbowDelimiterViolet",
+      "RainbowDelimiterCyan",
+    },
+  },
 }
