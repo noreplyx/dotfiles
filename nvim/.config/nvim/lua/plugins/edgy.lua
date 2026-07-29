7d9c212ed9348c2771e679a60909581a3fad05f7
Tanutchakorn <tanutchakornkhaisaeng@gmail.com>
2026-07-29 20:29:31 +0700

    nvim: add edgy.nvim sidebar layout manager

---
+++ nvim/.config/nvim/lua/plugins/edgy.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    opts = {
+      left = {
+        "aerial",
+        "neo-tree",
+        "neotest-summary",
+      },
+      right = {
+        "neogit",
+        "dbui",
+        "grug-far",
+        "diffview",
+      },
+      bottom = {
+        "trouble",
+        "qf",
+        "snacks_terminal",
+        "noice",
+        "help",
+        "neotest-output-panel",
+      },
+      keys = {
+        ["<c-q>"] = false,
+      },
+    },
+  },
+}
