e7ca59b976696bb949000ea1130ea99d471b4068
Tanutchakorn <tanutchakornkhaisaeng@gmail.com>
2026-07-30 01:54:14 +0700

    nvim: add <leader>oa keymap to select opencode agent

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("n", "<leader>oa", "<cmd>Opencode agent select<CR>", { desc = "Select agent" })
+
 vim.keymap.set("n", "<leader>fth", function()
   for _, win in ipairs(vim.api.nvim_list_wins()) do
     local buf = vim.api.nvim_win_get_buf(win)
