dbd06a65047a85c94142e7c4756bba502ca88251
Tanutchakorn <tanutchakorn@users.noreply.github.com>
2026-08-08 00:42:02 +0700

    nvim: add yazi.nvim plugin for file management

---
+++ nvim/.config/nvim/lua/plugins/yazi.nvim.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "mikavilpas/yazi.nvim",
+    version = "*",
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", lazy = true },
+    },
+    keys = {
+      {
+        "<leader>-",
+        mode = { "n", "v" },
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+      {
+        "<c-up>",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
+    },
+    opts = {
+      open_for_directories = false,
+      floating_window_scaling_factor = 0.9,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+  },
+}
