ad1825fd9ee14f9304f32d3e991c4b19dc4f6469
Tanutchakorn <tanutchakorn@users.noreply.github.com>
2026-08-07 21:09:01 +0700

    nvim: add smear-cursor plugin

---
+++ nvim/.config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      legacy_computing_symbols_support = false,
+      stiffness = 0.8,
+      trailing_stiffness = 0.6,
+      damping = 0.95,
+      smear_insert_mode = true,
+    },
+  },
+}
